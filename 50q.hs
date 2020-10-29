f179bbaeee106c595d58f81c3711152e4a95b99c
Cavalex <mateusmpta@gmail.com>
2020-10-29 23:57:08 +0000

    did some more questions, 31 to go

--- 50q.hs
+++ 50q.hs
@@ -63,6 +63,108 @@ intersperse2 _ [x] = [x]
 intersperse2 a (x:xs) = x : a : intersperse2 a xs
 
 --12
+-- The list must already be ordered for this to work
+{-
+group2 :: Eq a => [a] -> [[a]]
+group2 [] = []
+group2 [x] = [[x]]
+group2 l = groupList (makeList l)
+
+makeList :: Eq a =>  [a] -> [[a]]
+makeList [] = []
+makeList (x:xs) = [x] : makeList xs
+
+groupList :: Eq a => [[a]] -> [[a]]
+groupList [] = []
+groupList (x:y:xs)
+    | (head x == head y) && (tail y /= []) && (x /= []) = [head x, head y] : groupList ((tail y):xs)
+    | (head x /= head y) && (tail y /= []) && (x /= []) = x : groupList (y:xs)
+    | x == [] = groupList (y:xs)
+    | otherwise = [head x, head y] : groupList ([]:xs)
+groupList (x:xs) = [x]
+-}
+
+group3 :: Eq a => [a] -> [[a]]
+group3 [] = []
+group3 (x:xs) = (x:aux xs):aux2 xs
+    where
+        aux [] = []
+        aux (y:ys) = if x == y then (y : aux ys) else []
+        aux2 [] = []
+        aux2 (z:zs) = if z == x then aux2 zs else group3 (z:zs)
+
+--13
+concat3 :: [[a]] -> [a]
+concat3 [] = []
+concat3 ([]:xs) = concat3 xs
+concat3 ([x]:xs) = x : concat3 xs
+concat3 ((x:xs):ys) = x : concat3 (xs:ys)
+
+--14
+-- Jeez this is an ugly way to do it
+inits2 :: [a] -> [[a]]
+inits2 l = [] : reverse (aux2 (length l) l)
+    where
+        aux 0 l = []
+        aux n (x:xs) = x:aux (n-1) xs 
+        aux2 0 l = []
+        aux2 n l = (aux n l):(aux2 (n-1) l)
+
+-- better and simpler version:
+-- i tried this one first but forgot the [] on the last "l" and it wasn't working...
+inits3 :: [a] -> [[a]]
+inits3 [] = [[]]
+inits3 l = inits3 (init l) ++ [l]
+
+--15
+tails2 :: [a] -> [[a]]
+tails2 [] = [[]]
+tails2 l = l : tails2 (tail l)
+
+--16
+-- ignore this first function
+{-
+isPrefixOf2 :: Eq a => [a] -> [a] -> Bool
+isPrefixOf2 [] _ = True
+isPrefixOf2 _ [] = False
+isPrefixOf2 (x:xs) (y:ys)
+    | x == y = if test (x:xs) (y:ys) == True then True else isPrefixOf2 (x:xs) ys
+    | otherwise = isPrefixOf2 (x:xs) ys
+    where
+        test [] _ = True
+        test (z:zs) (t:ts) 
+            | z == t = test zs ts
+            | otherwise = False
+-}
+
+isPrefixOf3 :: Eq a => [a] -> [a] -> Bool
+isPrefixOf3 [] _ = True
+isPrefixOf3 _ [] = False
+isPrefixOf3 (x:xs) (y:ys) = x == y && isPrefixOf3 xs ys
+
+--17
+isSuffixOf2 :: Eq a => [a] -> [a] -> Bool
+isSuffixOf2 [] _ = True
+isSuffixOf2 _ [] = False
+isSuffixOf2 l1 l2 = last l1 == last l2 && isSuffixOf2 (init l1) (init l2)
+
+--18
+isSubsequenceOf2 :: Eq a => [a] -> [a] -> Bool
+isSubsequenceOf2 [] _ = True
+isSubsequenceOf2 _ [] = False
+isSubsequenceOf2 (x:xs) (y:ys)
+    | x == y = isSubsequenceOf2 xs ys
+    | otherwise = isSubsequenceOf2 (x:xs) (ys)
+
+--19
+elemIndices2 :: Eq a => a -> [a] -> [Int]
+elemIndices2 _ [] = []
+elemIndices2 n l = [x | x <- [0..(length l - 1)], l !! x == n]
+
+
+
+
+
 
 
 
